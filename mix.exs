194aed1ef7c6cc085d007ea9e1876c750e3df98c
damir <damir1petkovic@gmail.com>
2026-01-22 16:50:57 +0100

    update version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Extructure.MixProject do
   def project do
     [
       app: :extructure,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths( Mix.env()),
       start_permanent: Mix.env() == :prod,
